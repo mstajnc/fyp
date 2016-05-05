f14c647312499dffd5d8de98e08bc11e8091cc2a
mstajnc <u1276060@unimail.hud.ac.uk>
2016-05-05 21:20:04 +0100

    setup role relationship with permissions

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    // A role may be given many permissions.
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    // Helper method to grant the given permission to a role.
+    public function givePermissionTo(Permission $permission)
+    {
+        return $this->permissions()->save($permission);
+    }
 }
